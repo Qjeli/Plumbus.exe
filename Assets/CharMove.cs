3583408057e1b63c65b6995d0b5eafd0e136550a
unknown <orozalievali3@gmail.com>
2020-08-27 14:07:06 +0600

    Dialog

--- Assets/CharMove.cs
+++ Assets/CharMove.cs
@@ -4,15 +4,18 @@ using UnityEngine;
 
 public class CharMove : MonoBehaviour
 {
-    // Start is called before the first frame update
-    void Start()
-    {
-        
-    }
-
-    // Update is called once per frame
+    public CharacterController controller;
+    public float speed = 6f;
+    
     void Update()
     {
-        
+        float horizontal = Input.GetAxisRaw("Horizontal");
+        float vertical = Input.GetAxisRaw("Vertical");
+        Vector3 direction = new Vector3(horizontal, 0f, vertical).normalized;
+
+        if (direction.magnitude >= 0.1f)
+        {
+            controller.Move(direction * speed * Time.deltaTime);
+        }
     }
 }
---
+++ Assets/Dialogue.cs
@@ -0,0 +1,64 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class Dialogue : MonoBehaviour
+{
+    public DialogueNode[] node;
+    public int _currentNode;
+    public bool ShowDialogue = false;
+    public GameObject NPC;
+
+    void OnGUI()
+    {
+        if (ShowDialogue == true)
+        {
+            GUI.Box(new Rect(Screen.width / 2 - 300, Screen.height - 300, 600, 250), "");
+            GUI.Label(new Rect(Screen.width / 2 - 250, Screen.height - 280, 500, 90), node[_currentNode].NpcText);
+            for (int i = 0; i < node[_currentNode].PlayerAnswer.Length; i++)
+            {
+                if (GUI.Button(new Rect(Screen.width / 2 - 250, Screen.height - 200 + 25 * i, 500, 25), node[_currentNode].PlayerAnswer[i].Text))
+                {
+                    if (node[_currentNode].PlayerAnswer[i].SpeakEnd)
+                    {
+                        ShowDialogue = false;
+                    }
+                    _currentNode = node[_currentNode].PlayerAnswer[i].ToNode;
+                }
+            }
+        }
+    }
+
+    private void OnTriggerEnter(Collider other)
+    {
+        if (other.CompareTag("Player"))
+        {
+            ShowDialogue = true;
+        }
+    }
+
+    private void OnTriggerExit(Collider other)
+    {
+        if (other.CompareTag("Player"))
+        {
+            ShowDialogue = false;
+        }
+    }
+}
+
+[System.Serializable]
+public class DialogueNode
+{
+    public string NpcText;
+    public Answer[] PlayerAnswer;
+}
+
+
+[System.Serializable]
+public class Answer
+{
+    public string Text;
+    public int ToNode;
+    public bool SpeakEnd;
+}
